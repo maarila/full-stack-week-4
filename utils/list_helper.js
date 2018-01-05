c05ea61ee333a3aaec6966481a9940106748de04
Mika Äärilä <maarila@gmail.com>
2018-01-05 19:17:51 +0200

    tehtävä 62

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
   return 1;
-}
+};
+
+const totalLikes = (blogs) => {
+  return blogs.map((blog) => blog.likes).reduce((a, b) => {
+    return a + b;
+  }, 0);
+};
 
 module.exports = {
-  dummy
-}
+  dummy,
+  totalLikes
+};
