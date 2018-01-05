2d7d8a3df43b33a8f22c7eaeb38b1e9597f27337
Mika Äärilä <maarila@gmail.com>
2018-01-05 18:24:14 +0200

    tehtävä 60

--- index.js
+++ index.js
@@ -4,15 +4,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
-const Blog = mongoose.model("Blog", {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-module.exports = Blog;
+const Blog = require("./models/blog");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -22,19 +14,8 @@ const mongoUrl =
 mongoose.connect(mongoUrl);
 mongoose.Promise = global.Promise;
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+const blogsRouter = require("./controllers/blogs");
+app.use("/api/blogs", blogsRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
