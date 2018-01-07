6cad76ef70fd6faa765d4560e3f3170b96412f63
Mika Äärilä <maarila@gmail.com>
2018-01-07 15:43:06 +0200

    tehtävä 66

--- index.js
+++ index.js
@@ -5,19 +5,31 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Blog = require("./models/blog");
+const config = require("./utils/config");
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const mongoUrl =
-  "mongodb://blogilistadev:dev44j41@ds239217.mlab.com:39217/blogilista-dev";
-mongoose.connect(mongoUrl);
+const mongoUrl = "mongodb://localhost/bloglist";
+
+mongoose.connect(config.mongoUrl);
 mongoose.Promise = global.Promise;
 
 const blogsRouter = require("./controllers/blogs");
 app.use("/api/blogs", blogsRouter);
 
-const PORT = 3003;
-app.listen(PORT, () => {
+const PORT = config.port;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+server.on("close", () => {
+  mongoose.connection.close();
+});
+
+module.exports = {
+  app,
+  server
+};
